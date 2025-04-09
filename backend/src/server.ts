7168ade67612b275b3e62a148174ed0148d5f422
RashadMuhammad <rashad007muhammad@gmail.com>
2025-04-09 19:25:30 +0530

    final production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,12 +4,19 @@ import dotenv from "dotenv"
 import menuRoutes from "./routes/menuRoutes"
 import menuItemRoutes from "./routes/menuItemRoutes"
 import connectDB from "./config/connectDB"
+import path from "path"
 
 dotenv.config()
 
 const app: Express = express()
 const PORT: number = Number.parseInt(process.env.PORT || "5000", 10)
 const CLIENT_URL = process.env.CLIENT_URL
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Middleware
 app.use(express.json())
